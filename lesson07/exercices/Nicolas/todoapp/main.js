2ea0195bf33b7b726f8b6ff889d61ca3cbcbdce2
nbienaime <nicolas.bienaime@niji.fr>
2015-12-05 17:48:13 +0100

    Add todo list

--- lesson07/exercices/Nicolas/todoapp/main.js
+++ lesson07/exercices/Nicolas/todoapp/main.js
@@ -1,31 +1,48 @@
-var todos = [];
+// localStorage.clear();
+if(localStorage.length == 0){
+	localStorage.setItem('todos','[]');
+}
+
+var todos = JSON.parse(localStorage.todos);
 var field = document.querySelector('.new-todo');
 var list = document.querySelector('.todo-list');
 
+function checkList(){
+	if(todos.length != 0){
+		for(var i = 0; i < todos.length; i++){
+			var item = document.createElement('li');
+			item.textContent = todos[i];
+			list.appendChild(item);
+		}
+	}
+}
+
 function addItem(itemContent){
 	todos.push(itemContent);
-	console.log(todos);
+	localStorage.todos = JSON.stringify(todos);
 }
 
 function deleteItem(itemContent){
 	for(var i = 0; i < todos.length; i++){
 		if(itemContent == todos[i]){
 			todos.splice(i,1);
+			localStorage.todos = JSON.stringify(todos);
 		}
 	}
-	console.log(todos);
 }
 
 function returnTodos(){
-	return todos;
+	return localStorage.todos;
 }
+
+checkList();
 
 field.addEventListener('keypress', function (e){
 	if(e.keyCode == 13 && field.value != ''){
 
 		var item = document.createElement('li');
 		item.textContent = field.value;
-		item.className = 'todo-item';
+		// item.className = 'todo-item';
 		list.appendChild(item);
 
 		field.value = '';
