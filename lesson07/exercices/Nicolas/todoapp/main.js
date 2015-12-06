d6f5450e07e63eb0701e30903c8bbbc95b765acb
nbienaime <nicolas.bienaime@niji.fr>
2015-12-06 15:45:17 +0100

    Improved todo list

--- lesson07/exercices/Nicolas/todoapp/main.js
+++ lesson07/exercices/Nicolas/todoapp/main.js
@@ -11,8 +11,19 @@ function checkList(){
 	if(todos.length != 0){
 		for(var i = 0; i < todos.length; i++){
 			var item = document.createElement('li');
+			var button = document.createElement('button');
+
 			item.textContent = todos[i];
+
 			list.appendChild(item);
+			item.appendChild(button);
+
+			button.addEventListener('click', function (e){
+				var item = e.target.parentNode;
+				list.removeChild(item);
+
+				deleteItem(item.textContent);
+			}, false);
 		}
 	}
 }
@@ -39,20 +50,22 @@ checkList();
 
 field.addEventListener('keypress', function (e){
 	if(e.keyCode == 13 && field.value != ''){
-
 		var item = document.createElement('li');
+		var button = document.createElement('button');
+
 		item.textContent = field.value;
-		// item.className = 'todo-item';
+
 		list.appendChild(item);
+		item.appendChild(button);
 
 		field.value = '';
 		addItem(item.textContent);
+
+		button.addEventListener('click', function (e){
+			var item = e.target.parentNode;
+			list.removeChild(item);
+
+			deleteItem(item.textContent);
+		}, false);
 	}
-}, false);
-
-list.addEventListener('click', function (e){
-	var item = e.target;
-	list.removeChild(item);
-
-	deleteItem(item.textContent);
 }, false);
