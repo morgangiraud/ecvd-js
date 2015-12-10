bd7a9ef8cb06f68c5517c60f6a67e3db34b75dac
ECV DIGITAL 04 <ecvdigital04@ECV-PA-D001-IM004.local>
2015-12-10 19:23:55 +0100

    Exercise lesson 09

---
+++ lesson08/exercices/Nicolas/todoapp/main.js
@@ -0,0 +1,68 @@
+if(localStorage.length == 0){
+	localStorage.setItem('todos','[]');
+}
+
+var todos = JSON.parse(localStorage.todos);
+var field = document.querySelector('.new-todo');
+var list = document.querySelector('.todo-list');
+
+function addItem(itemContent){
+	todos.push(itemContent);
+	localStorage.todos = JSON.stringify(todos);
+}
+
+function deleteItem(itemContent){
+	for(var i = 0; i < todos.length; i++){
+		if(itemContent == todos[i]){
+			todos.splice(i,1);
+			localStorage.todos = JSON.stringify(todos);
+		}
+	}
+}
+
+function returnTodos(){
+	return localStorage.todos;
+}
+
+if(todos.length != 0){
+	for(var i = 0; i < todos.length; i++){
+		var item = document.createElement('li');
+		var button = document.createElement('button');
+
+		item.textContent = todos[i];
+
+		list.appendChild(item);
+		item.appendChild(button);
+
+		button.addEventListener('click', function (e){
+			var item = e.target.parentNode;
+			list.removeChild(item);
+
+			deleteItem(item.textContent);
+		}, false);
+	}
+}
+
+field.addEventListener('keypress', function (e){
+	if(e.keyCode == 13 && field.value != ''){
+		var item = document.createElement('li');
+		var span = document.createElement('span');
+		var button = document.createElement('button');
+
+		span.textContent = field.value;
+
+		list.appendChild(item);
+		item.appendChild(span);
+		item.appendChild(button);
+
+		field.value = '';
+		addItem(item.textContent);
+
+		button.addEventListener('click', function (e){
+			var item = e.target.parentNode;
+			list.removeChild(item);
+
+			deleteItem(item.textContent);
+		}, false);
+	}
+}, false);
