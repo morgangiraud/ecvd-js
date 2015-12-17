b21abfb3d2f988adf7b3ef92ef7354e74d657797
ECV DIGITAL 05 <ecvdigital05@ECV-PA-D001-IM005.local>
2015-12-17 14:51:07 +0100

    Add algo

---
+++ finaltest/ludivine/exercices/exercice1.js
@@ -0,0 +1,19 @@
+function aPowerN(a, n){
+
+	var a;
+	var n = Math.pow();
+
+	for () {
+
+ 		a * n;
+	}
+}
+// Complexity:
+
+// --------------------------------------------------------
+// Check your algorithm
+console.log(aPowerN(0, 0) === Math.pow(0, 0));
+console.log(aPowerN(0, 1) === Math.pow(0, 1));
+console.log(aPowerN(1, 8) === Math.pow(1, 8));
+console.log(aPowerN(5, 12) === Math.pow(5, 12));
+console.log(aPowerN(34567, 2) === Math.pow(34567, 2));
